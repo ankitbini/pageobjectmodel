03553c3aeefa8de1719e2d43f3a63637f4f4ed78
ADS\t_shara <ankit.kumar.sharma@autodesk.com>
2018-12-25 13:03:59 +0530

    remove space

--- src/test/java/com/autodesk/rough/LiginTest.java
+++ src/test/java/com/autodesk/rough/LiginTest.java
@@ -12,7 +12,6 @@ public class LiginTest {
 
 	public static void main(String[] args) {
 		HomePage home = new HomePage();
-		//some rough work
 		LoginPage login = home.goToLogin();
 		ZohoAppPage appPage = login.doLoginIn("akgankitsharma@gmail.com", "GHAR!wsXm2s");
 		appPage.goToCRM();
